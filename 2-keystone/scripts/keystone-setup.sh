8ca6f1016a8e5db87d9e66b4b8ba79736a7d6285
Arturo Rinaldi <arty.net2@gmail.com>
2021-11-20 01:59:28 +0100

    2-keystone: adding execution check for keystone-setup.sh bash script

--- 2-keystone/scripts/keystone-setup.sh
+++ 2-keystone/scripts/keystone-setup.sh
@@ -11,26 +11,33 @@ fi
 
 echo $URL
 
-keystone-manage db_sync
-echo "db_sync"
+if [ ! -f ${HOME}/.keystone_setup_completed ]
+then
+    keystone-manage db_sync
+    echo "db_sync"
 
-keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
-echo "fernet_setup"
+    keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
+    echo "fernet_setup"
 
-keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
-echo "credential_setup"
+    keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
+    echo "credential_setup"
 
-keystone-manage bootstrap --bootstrap-password ${ADMIN_PASS} --bootstrap-admin-url ${URL} \
-	--bootstrap-internal-url ${URL} --bootstrap-public-url ${URL} --bootstrap-region-id RegionOne
-echo "boostrap"
+    keystone-manage bootstrap --bootstrap-password ${ADMIN_PASS} --bootstrap-admin-url ${URL} \
+        --bootstrap-internal-url ${URL} --bootstrap-public-url ${URL} --bootstrap-region-id RegionOne
+    echo "boostrap"
 
-# restarting apache2 server
-apachectl -k graceful
+    # restarting apache2 server
+    apachectl -k graceful
 
-openstack project create --domain default  --description "Service Project" service
-echo "project_created"
+    openstack project create --domain default  --description "Service Project" service
+    echo "project_created"
 
-chown -R keystone:keystone /var/log/keystone/
-echo "permissions_to_var_log_keystone"
+    chown -R keystone:keystone /var/log/keystone/
+    echo "permissions_to_var_log_keystone"
 
-echo "restart the container"
+    touch ${HOME}/.keystone_setup_completed
+
+    echo "restart the container"
+else
+    echo "no need to setup again keystone !"
+fi
