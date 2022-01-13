32a794fbba6119cb32ec86c5cef2a2957483b463
Arturo Rinaldi <arty.net2@gmail.com>
2022-01-13 15:32:06 +0100

    7-designate: rework of designate image and scripts

---
+++ 7-designate/build/scripts/designate-setup.sh
@@ -0,0 +1,23 @@
+IP_DESIGNATE="demo-controller.smartme.io"
+HTTPS=true
+DESIGNATE_PASS="smartme"
+
+URL="http://$IP_DESIGNATE:9001"
+if [ "$HTTPS" = true ] ; then
+    URL="https://$IP_DESIGNATE:9001"
+fi
+
+echo $URL
+
+if [ ! -e ${HOME}/.designate_setup_completed ]
+then
+  openstack service create dns --name Designate
+  openstack user create --password $DESIGNATE_PASS designate
+  openstack role add --project service --user designate admin
+
+  openstack endpoint create --region RegionOne dns public $URL
+  
+  touch ${HOME}/.designate_setup_completed
+else
+	echo "no need to setup designate !"
+fi
